51bea26fdb5d361567591e38fa4a3041b219326d
Alan Robinson <alanrobinson1337@gmail.com>
2021-04-14 12:03:01 +0100

    Start 4.2

--- TangoWithDjango/settings.py
+++ TangoWithDjango/settings.py
@@ -12,9 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+
+
+print(__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -52,10 +59,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'TangoWithDjango.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+print(TEMPLATE_DIR)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [TEMPLATE_DIR, ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!" 
-                        "<a href='/rango/about/'>About</a>")
+    #return HttpResponse("Rango says hey there partner!"  "<a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Cruncy, creamy, cooking, andy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("This is the rango about page partner"

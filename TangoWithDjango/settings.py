6d5d744733851e24d08032f99a31e7b1c1d42a6b
Alan Robinson <alanrobinson1337@gmail.com>
2021-04-21 11:22:58 +0100

    change Base_DIR

--- TangoWithDjango/settings.py
+++ TangoWithDjango/settings.py
@@ -19,7 +19,8 @@ print(os.path.dirname(__file__))
 print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 print(BASE_DIR)
 

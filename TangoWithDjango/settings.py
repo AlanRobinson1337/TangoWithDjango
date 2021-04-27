1ce575e54c61cfb70652bc8abfc73e1a875d49c1
Alan Robinson <alanrobinson1337@gmail.com>
2021-04-27 17:16:03 +0100

    Start 4.4

--- TangoWithDjango/settings.py
+++ TangoWithDjango/settings.py
@@ -20,8 +20,17 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 #print(BASE_DIR)
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+#print(TEMPLATE_DIR)
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDAI_URL ='/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -59,13 +68,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'TangoWithDjango.urls'
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [STATIC_DIR, ]
-
-#print(TEMPLATE_DIR)
 
 TEMPLATES = [
     {
@@ -79,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
